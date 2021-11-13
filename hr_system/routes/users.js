4b8b4e5f6f8fba4fbcab1beb0b4c729ec018f56f
pythexcel <pythexcel@excellencetechnologies.in>
2021-11-13 10:32:56 +0100

    assigned new role

--- hr_system/routes/users.js
+++ hr_system/routes/users.js
@@ -4,15 +4,14 @@ const validators = require('../validators/req-validators');
 const userController = require('../controllers/user-controller');
 const handlers = require('../util/responseHandlers');
 const middleware = require("../middleware/Auth");
-
-router.post('/register', validators.userCreationValidator, userController.userRegister,handlers.responseForData);
-router.post('/login',validators.userLoginValidator,userController.userLogin,handlers.responseHandle);
-router.post('/add_roles',middleware.AuthForAdmin,validators.addRoleValidator,userController.addUserRole,handlers.addNewEmployeeResponseHandle);
-router.get('/list_all_roles',middleware.AuthForAdmin,userController.getUserRole,handlers.resForList);
-router.post("/add_new_employee", middleware.AuthForHr, validators.addNewEmployeeValidator ,userController.addNewEmployeeController, handlers.addNewEmployeeResponseHandle);
-router.post("/assign_user_role", middleware.AuthForAdmin, validators.assignUserRoleValidator, userController.assignUserRoleController, handlers.addNewEmployeeResponseHandle);
-router.post("/get_enable_user", middleware.AuthForAdmin, userController.getEnableUser, handlers.newResponse);
-router.post("/update_role", middleware.AuthForAdmin, validators.updateRoleValidators, userController.updateRoleController, handlers.responseHandle);
+ 
+router.post('/register', validators.userCreationValidator, userController.userRegister,handlers.responseForData); //done
+router.post('/login',validators.userLoginValidator,userController.userLogin,handlers.responseHandle); //done
+router.post('/add_roles',middleware.AuthForAdmin,validators.addRoleValidator,userController.addUserRole,handlers.addNewEmployeeResponseHandle); //done
+router.get('/list_all_roles',middleware.AuthForAdmin,userController.getUserRole,handlers.resForList); //done
+router.post("/add_new_employee", middleware.AuthForHr, validators.addNewEmployeeValidator ,userController.addNewEmployeeController, handlers.addNewEmployeeResponseHandle); //done
+router.post("/assign_user_role", middleware.AuthForAdmin, validators.assignUserRoleValidator, userController.assignUserRoleController, handlers.addNewEmployeeResponseHandle); //done
+router.post("/get_enable_user", middleware.AuthForAdmin, userController.getEnableUser, handlers.newResponse); //done
+router.post("/update_role", middleware.AuthForAdmin, validators.updateRoleValidators, userController.updateRoleController, handlers.responseHandle); //done
 // router.get("/list_all_roles", middleware.AuthForHr, userController.listAllRolesController, handlers.responseHandle);
 module.exports = router;
- 
